93d5f16c8c706cb607d471aa200f82988bf58ffb
GABRIEL ALMEIDA LISBOA DE ARIAN <gabriel.alarian@senacsp.edu.br>
2023-08-31 17:14:33 -0300

    Aplicação Ti110

---
+++ EmpresaABC/MercadoSA/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace MercadoSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.cbbProdutos = new System.Windows.Forms.ComboBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lstProdutos = new System.Windows.Forms.ListBox();
+            this.ckbLivro = new System.Windows.Forms.CheckBox();
+            this.rdbFeminino = new System.Windows.Forms.RadioButton();
+            this.dtpcalendario = new System.Windows.Forms.DateTimePicker();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(53, 48);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(33, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "nome";
+            this.lblNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbbProdutos
+            // 
+            this.cbbProdutos.FormattingEnabled = true;
+            this.cbbProdutos.Location = new System.Drawing.Point(485, 77);
+            this.cbbProdutos.Name = "cbbProdutos";
+            this.cbbProdutos.Size = new System.Drawing.Size(121, 21);
+            this.cbbProdutos.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(56, 135);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // lstProdutos
+            // 
+            this.lstProdutos.FormattingEnabled = true;
+            this.lstProdutos.Location = new System.Drawing.Point(70, 245);
+            this.lstProdutos.Name = "lstProdutos";
+            this.lstProdutos.Size = new System.Drawing.Size(120, 95);
+            this.lstProdutos.TabIndex = 3;
+            // 
+            // ckbLivro
+            // 
+            this.ckbLivro.AutoSize = true;
+            this.ckbLivro.Location = new System.Drawing.Point(524, 32);
+            this.ckbLivro.Name = "ckbLivro";
+            this.ckbLivro.Size = new System.Drawing.Size(49, 17);
+            this.ckbLivro.TabIndex = 4;
+            this.ckbLivro.Text = "Livro";
+            this.ckbLivro.UseVisualStyleBackColor = true;
+            this.ckbLivro.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // rdbFeminino
+            // 
+            this.rdbFeminino.AutoSize = true;
+            this.rdbFeminino.Location = new System.Drawing.Point(506, 282);
+            this.rdbFeminino.Name = "rdbFeminino";
+            this.rdbFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rdbFeminino.TabIndex = 5;
+            this.rdbFeminino.TabStop = true;
+            this.rdbFeminino.Text = "Feminino";
+            this.rdbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // dtpcalendario
+            // 
+            this.dtpcalendario.Location = new System.Drawing.Point(485, 172);
+            this.dtpcalendario.Name = "dtpcalendario";
+            this.dtpcalendario.Size = new System.Drawing.Size(200, 20);
+            this.dtpcalendario.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(56, 78);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.dtpcalendario);
+            this.Controls.Add(this.rdbFeminino);
+            this.Controls.Add(this.ckbLivro);
+            this.Controls.Add(this.lstProdutos);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.cbbProdutos);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.ComboBox cbbProdutos;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ListBox lstProdutos;
+        private System.Windows.Forms.CheckBox ckbLivro;
+        private System.Windows.Forms.RadioButton rdbFeminino;
+        private System.Windows.Forms.DateTimePicker dtpcalendario;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
